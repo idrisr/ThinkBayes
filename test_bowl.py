e2a8cc7c1b4cede2d643c0b8a2b51783a9c121ff
Idris Raja <idris@viglink.com>
2013-11-06 18:16:22 -0800

    test files for ex 2.8

---
+++ test_bowl.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+__author__ = 'idris'
+
+
+class TestBowl(TestCase):
+    pass
---
+++ test_cookie.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+__author__ = 'idris'
+
+
+class TestCookie(TestCase):
+    def test_Update(self):
+        self.fail()
+
+    def test_Likelihood(self):
+        self.fail()
